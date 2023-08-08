8d93899a90cd4f655d7e1eb006b921c4ab2d8efc
Crowdbotics <team@crowdbotics.com>
2023-08-08 13:32:37 +0000

    Initial commit.

---
+++ backend/mrm_version1_42726/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mrm_version1_42726 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mrm_version1_42726.settings')
+
+application = get_wsgi_application()
